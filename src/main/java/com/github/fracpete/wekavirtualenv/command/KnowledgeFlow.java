0c851991632d832b6c869d474edcc11e77c25f82
Peter Reutemann <fracpete@gmail.com>
2018-01-07 20:26:08 +1300

    added command/action for KnowledgeFlow

---
+++ src/main/java/com/github/fracpete/wekavirtualenv/command/KnowledgeFlow.java
@@ -0,0 +1,68 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * KnowledgeFlow.java
+ * Copyright (C) 2017-2018 University of Waikato, Hamilton, NZ
+ */
+
+package com.github.fracpete.wekavirtualenv.command;
+
+import com.github.fracpete.simpleargparse4j.Namespace;
+import com.github.fracpete.wekavirtualenv.env.Environment;
+
+/**
+ * Launches the Weka KnowledgeFlow.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class KnowledgeFlow
+  extends AbstractLaunchCommand {
+
+  /**
+   * The name of the command (used on the commandline).
+   *
+   * @return		the name
+   */
+  @Override
+  public String getName() {
+    return "knowledgeflow";
+  }
+
+  /**
+   * Returns a short help string.
+   *
+   * @return		the help string
+   */
+  public String getHelp() {
+    return "Launches the Weka KnowledgeFlow.";
+  }
+
+  /**
+   * Executes the command.
+   *
+   * @param ns		the namespace of the parsed options, null if no options to parse
+   * @param options	additional command-line options
+   * @return		true if successful
+   */
+  protected boolean doExecute(Namespace ns, String[] options) {
+    String  classname;
+
+    classname = "weka.gui.knowledgeflow.KnowledgeFlow";
+    if (!Environment.hasClass(getEnv().weka, classname, true))
+      classname = "weka.gui.beans.KnowledgeFlow";
+    return launch(build(classname, options));
+  }
+}
---
+++ src/main/java/com/github/fracpete/wekavirtualenv/gui/action/KnowledgeFlow.java
@@ -0,0 +1,93 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * KnowledgeFlow.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, NZ
+ */
+
+package com.github.fracpete.wekavirtualenv.gui.action;
+
+/**
+ * Starts the KnowledgeFlow.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class KnowledgeFlow
+  extends AbstractEnvironmentAction {
+
+  /** the command. */
+  protected com.github.fracpete.wekavirtualenv.command.KnowledgeFlow m_Command;
+
+  /**
+   * Returns the name of the action (displayed in GUI).
+   *
+   * @return		the name
+   */
+  @Override
+  public String getName() {
+    return "KnowledgeFlow";
+  }
+
+  /**
+   * The group this action belongs to.
+   *
+   * @return		the group
+   */
+  @Override
+  public String getGroup() {
+    return "gui";
+  }
+
+  /**
+   * Returns whether the action generates console output.
+   *
+   * @return		true if the action generates console output
+   */
+  public boolean generatesOutput() {
+    return true;
+  }
+
+  /**
+   * Performs the actual execution.
+   *
+   * @return		null if successful, otherwise error message
+   */
+  @Override
+  protected String doExecute() {
+    String	result;
+
+    result    = null;
+    m_Command = new com.github.fracpete.wekavirtualenv.command.KnowledgeFlow();
+    m_Command.setEnv(m_Environment);
+    transferOutputListeners(m_Command);
+    if (!m_Command.execute(new String[0])) {
+      if (m_Command.hasErrors())
+        result = m_Command.getErrors();
+      else
+        result = "Failed to launch KnowledgeFlow!";
+    }
+    m_Command = null;
+    return result;
+  }
+
+  /**
+   * Destroys the process if possible.
+   */
+  public void destroy() {
+    if (m_Command != null)
+      m_Command.destroy();
+  }
+}
