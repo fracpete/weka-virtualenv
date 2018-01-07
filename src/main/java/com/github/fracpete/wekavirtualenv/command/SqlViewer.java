2bbfe6c28e4ea806a54e8da7abd3bed4b7c7c96d
Peter Reutemann <fracpete@gmail.com>
2018-01-07 19:57:42 +1300

    added command/action for SqlViewer

---
+++ src/main/java/com/github/fracpete/wekavirtualenv/command/SqlViewer.java
@@ -0,0 +1,62 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * SqlViewer.java
+ * Copyright (C) 2018 University of Waikato, Hamilton, NZ
+ */
+
+package com.github.fracpete.wekavirtualenv.command;
+
+import com.github.fracpete.simpleargparse4j.Namespace;
+
+/**
+ * Launches the Weka SqlViewer.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class SqlViewer
+  extends AbstractLaunchCommand {
+
+  /**
+   * The name of the command (used on the commandline).
+   *
+   * @return		the name
+   */
+  @Override
+  public String getName() {
+    return "sqlviewer";
+  }
+
+  /**
+   * Returns a short help string.
+   *
+   * @return		the help string
+   */
+  public String getHelp() {
+    return "Launches the Weka SQL Viewer.";
+  }
+
+  /**
+   * Executes the command.
+   *
+   * @param ns		the namespace of the parsed options, null if no options to parse
+   * @param options	additional command-line options
+   * @return		true if successful
+   */
+  protected boolean doExecute(Namespace ns, String[] options) {
+    return launch(build("weka.gui.sql.SqlViewer", options));
+  }
+}
